3e48f665b53ba58e6b04b685510a659acd3420d7
mcomolli <max.comolli@gmail.com>
2021-05-10 11:02:34 -0700

    Create confusion_matrix.py

---
+++ confusion_matrix.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+from IPython.display import display
+
+pred_test = np.asarray([[1, 0, 0], [1, 0, 0]])
+label_test = np.asarray([[0, 1, 0], [0, 1, 0]])
+
+def main():
+    np.savetxt("test.csv", confusion_matrix(pred_test, label_test), delimiter=',')
+
+# convert softmax output to binary values
+def format_predictions(softmax_output):
+    res = np.zeros(softmax_output.shape)
+    for i in range(softmax_output.shape[0]):
+        res[i,:] = softmax_output[i,:] = np.max(softmax_output[i,:])
+    return res
+        
+
+# return confusion matrix
+def confusion_matrix(preds, labels):
+    num_ex = labels.shape[0]
+    predicted_labels = format_predictions(preds)
+    width = labels.shape[1]
+    height = width
+    res = np.zeros((width, height))
+    for i in range(num_ex):
+        row = np.argmax(labels[i,:])
+        col = np.argmax(preds[i:,])
+        res[row,col] += 1
+    return res.astype(int)
+    
+
+
+
+if __name__ == "__main__":
+    main()
