ff588e037c9d8af511a1e48ff01a1e8cb98a9eb9
tcwelch <43589658+tcwelch@users.noreply.github.com>
2021-05-12 14:42:00 -0700

    improved accuracy!

--- transfer_learning1.py
+++ transfer_learning1.py
@@ -13,10 +13,10 @@ def main():
 	# Run training data through RestNet50
 	transfer_model = tf.keras.applications.ResNet50(include_top=False, weights="imagenet")
 	A_transfer = transfer_model.predict(util.normalize(X_train)) # sized at (m,7,7,2048)
-	A_transfer = np.reshape(A_transfer,(m,-1)) # sized at (m,7*7*2048)
+	A_transfer = np.reshape(util.normalize(A_transfer),(m,-1)) # sized at (m,7*7*2048)
 
 	# Train our model on output of Resnet50
-	our_model = util.neural_network(A_transfer, y_train,[1024,256,64,16,6],epochs = 15,lambd=1e-4)
+	our_model = util.neural_network(A_transfer, y_train,[1024,256,64,16,6],epochs = 50,lambd=1e-4)
 	print(our_model.summary())
 
 	# Prediction on training data
@@ -29,7 +29,7 @@ def main():
 	X_valid, y_valid = data_proc.get_batch('archive/one-indexed-files-notrash_val.txt',224,224)
 	m = X_valid.shape[0]
 	A_transfer = transfer_model.predict(util.normalize(X_valid)) # sized at (m,7,7,2048)
-	A_transfer = np.reshape(A_transfer,(m,-1)) # sized at (m,7*7*2048)
+	A_transfer = np.reshape(util.normalize(A_transfer),(m,-1)) # sized at (m,7*7*2048)
 
 	# Prediction on validation data
 	y_valid_pred = our_model.predict(A_transfer)
@@ -37,10 +37,10 @@ def main():
 	np.savetxt("confusion_matrix_valid_ResNet.csv", confusion_matrix.generate_confusion_matrix(y_valid_pred, y_valid), delimiter=',')
 
 	# -------- Printing metrics for Training and Validation -------
-	acc = util.compute_accuracy(y_train_pred, y_train)
-	prec = util.compute_ma_precision(y_train_pred, y_train)
-	recall = util.compute_ma_recall(y_train_pred, y_train)
-	F1 = util.compute_ma_F1Score(y_train_pred, y_train)
+	acc = confusion_matrix.compute_accuracy(y_train_pred, y_train)
+	prec = confusion_matrix.compute_ma_precision(y_train_pred, y_train)
+	recall = confusion_matrix.compute_ma_recall(y_train_pred, y_train)
+	F1 = confusion_matrix.compute_ma_F1Score(y_train_pred, y_train)
 
 	print("Metrics for Training - ResNet50:")
 	print(f"Accuracy = {acc}")
@@ -48,10 +48,10 @@ def main():
 	print(f"Recall = {recall}")
 	print(f"F1 Score = {F1}")
 
-	acc = util.compute_accuracy(y_valid_pred, y_valid)
-	prec = util.compute_ma_precision(y_valid_pred, y_valid)
-	recall = util.compute_ma_recall(y_valid_pred, y_valid)
-	F1 = util.compute_ma_F1Score(y_valid_pred, y_valid)
+	acc = confusion_matrix.compute_accuracy(y_valid_pred, y_valid)
+	prec = confusion_matrix.compute_ma_precision(y_valid_pred, y_valid)
+	recall = confusion_matrix.compute_ma_recall(y_valid_pred, y_valid)
+	F1 = confusion_matrix.compute_ma_F1Score(y_valid_pred, y_valid)
 
 	print("Metrics for Validation - ResNet50:")
 	print(f"Accuracy = {acc}")
